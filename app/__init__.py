c077dffcdd77b5a1e85e6f6cfb7f5b7a1fef9b57
Mupa M <mupasmail@gmail.com>
2019-01-10 10:47:34 +0300

    [chore #163110519] modify app __init__  file

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 import os
-from flask import Blueprint, Flask
-from app.api.v1.routes import VERSION_ONE as v1
+from flask import Flask
 
 
 from app.instance.config import APP_CONFIG
@@ -10,6 +9,6 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(APP_CONFIG[config_name])
 
-    app.register_blueprint(v1)
+   
 
     return app
