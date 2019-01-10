e67d94b431ce9c6842faf2b10aad0a5a4a54cfc6
Mupa M <mupasmail@gmail.com>
2019-01-10 16:16:43 +0300

    [feature #163115366] modify views file

--- app/api/v1/meetups/views.py
+++ app/api/v1/meetups/views.py
@@ -26,3 +26,15 @@ class Meetups(Resource):
                 "tags":request.json.get('tags')
             }
         })
+
+    def get(self):
+        """docstring for fetching all the meetups posted"""
+        self.db.get_all()
+        return jsonify({
+            'location': request.json.get('location'),
+            'title': request.json.get('title'),
+            'happeningOn': request.json.get('happeningOn'),
+            'tags': request.json.get('tags')
+            "status": 200,
+            "data": self.db.get_all()
+        })
